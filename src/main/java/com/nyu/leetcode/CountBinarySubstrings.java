0ab5cbdc594bdfe8754b89625a91e347bd1dcbeb
Hao Dong <13132193357@163.com>
2021-11-20 23:41:13 -0500

    CountBinarySubstrings

---
+++ src/main/java/com/nyu/leetcode/CountBinarySubstrings.java
@@ -0,0 +1,31 @@
+package com.nyu.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountBinarySubstrings{
+
+    public static int countBinarySubstrings(String s) {
+        List<Integer> counts = new ArrayList<Integer>();
+        int ptr = 0, n = s.length();
+        while (ptr < n) {
+            char c = s.charAt(ptr);
+            int count = 0;
+            while (ptr < n && s.charAt(ptr) == c) {
+                ++ptr;
+                ++count;
+            }
+            counts.add(count);
+        }
+        int ans = 0;
+        for (int i = 1; i < counts.size(); ++i) {
+            ans += Math.min(counts.get(i), counts.get(i - 1));
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String s = "10101";
+        System.out.println(countBinarySubstrings(s));
+    }
+}
