012bb0f0903e2407ec76db5dc50c0baa04d6f5c2
Hao Dong <13132193357@163.com>
2022-01-18 23:28:52 -0500

    leetcode

---
+++ src/main/java/com/nyu/leetcode/ContainsDuplicateII.java
@@ -0,0 +1,28 @@
+package com.nyu.leetcode;
+
+public class ContainsDuplicateII {
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        int length = nums.length;
+        boolean flag = false;
+        for (int i = 0; i < length; i++) {
+            for (int j = 1; j <= k; j++) {
+                if ( i + j > length - 1) {
+                    break;
+                }
+                if (nums[i] == nums[i + j]) {
+                    flag = true;
+                    return flag;
+                }
+            }
+        }
+        return flag;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,0,1,1};
+        int k = 1;
+        System.out.println(containsNearbyDuplicate(nums, k));
+    }
+}
