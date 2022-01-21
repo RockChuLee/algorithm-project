ce9f8a21f0e1e8a3d5de06337b47406621ff20be
Hao Dong <13132193357@163.com>
2022-01-20 23:10:38 -0500

    leetcode

---
+++ src/main/java/com/nyu/datastructure/linkedlist/LinkedListCycle.java
@@ -0,0 +1,39 @@
+package com.nyu.datastructure.linkedlist;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LinkedListCycle {
+
+    //
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> seen = new HashSet<ListNode>();
+        while (head != null) {
+            if (!seen.add(head)) {
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+
+    public boolean hasCycle1(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+
+
+    public LinkedListCycle() {
+    }
+}
---
+++ src/main/java/com/nyu/datastructure/linkedlist/RemoveLinkedListElements.java
@@ -0,0 +1,27 @@
+package com.nyu.datastructure.linkedlist;
+
+public class RemoveLinkedListElements {
+    public ListNode removeElements(ListNode head, int val) {
+
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode temp = dummyHead;
+        while (temp.next != null) {
+            if (temp.next.val == val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return dummyHead.next;
+
+    }
+
+    public ListNode removeElements1(ListNode head, int val) {
+        if (head == null) {
+            return head;
+        }
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;
+    }
+}
