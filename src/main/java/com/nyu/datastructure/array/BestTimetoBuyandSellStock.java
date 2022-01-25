fd9996b80a05913f2de65033919de6af64e2952b
Hao Dong <13132193357@163.com>
2022-01-24 23:30:03 -0500

    array

---
+++ src/main/java/com/nyu/datastructure/array/BestTimetoBuyandSellStock.java
@@ -0,0 +1,36 @@
+package com.nyu.datastructure.array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BestTimetoBuyandSellStock {
+
+    //brute force
+    public int maxProfit(int prices[]) {
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxprofit) {
+                    maxprofit = profit;
+                }
+            }
+        }
+        return maxprofit;
+    }
+
+    public int maxProfit1(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice) {
+                minprice = prices[i];
+            } else if (prices[i] - minprice > maxprofit) {
+                maxprofit = prices[i] - minprice;
+            }
+        }
+        return maxprofit;
+    }
+
+}
--- src/main/java/com/nyu/datastructure/array/Test.java
+++ src/main/java/com/nyu/datastructure/array/Test.java
@@ -2,10 +2,13 @@ package com.nyu.datastructure.array;
 
 public class Test {
     static int[] nums = new int[]{-2,4,-1,2};
+    static int[] nums1 = new int[]{1,2,3,0,0,0};
+    static int[] nums2 = new int[]{2,5,6};
 
     public static void main(String[] args) {
 //        containsDuplicate();
-        maximumSubarray();
+//        maximumSubarray();
+        merge();
     }
 
     private static void maximumSubarray() {
@@ -14,5 +17,9 @@ public class Test {
 
     private static void containsDuplicate() {
         System.out.println("ContainsDuplicate : " + ContainsDuplicate.containsDuplicate(nums));
+    }
+
+    private static void merge() {
+        MergeSortedArray.merge1(nums1,3,nums2,3);
     }
 }
