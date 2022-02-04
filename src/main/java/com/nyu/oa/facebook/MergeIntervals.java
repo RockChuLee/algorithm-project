0d03a553e2af79ab61a10e1cc1fdb3460c7fbbe8
Hao Dong <13132193357@163.com>
2022-02-03 23:41:24 -0500

    facebook tag

---
+++ src/main/java/com/nyu/oa/facebook/MergeIntervals.java
@@ -0,0 +1,34 @@
+package com.nyu.oa.facebook;
+
+import java.util.*;
+
+//https://leetcode-cn.com/problems/merge-intervals/
+public class MergeIntervals {
+    public static int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) {
+            return new int[0][2];
+        }
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] interval1, int[] interval2) {
+                return interval1[0] - interval2[0];
+            }
+        });
+        List<int[]> merged = new ArrayList<int[]>();
+        for (int i = 0; i < intervals.length; ++i) {
+            int L = intervals[i][0], R = intervals[i][1];
+            if (merged.size() == 0 || merged.get(merged.size() - 1)[1] < L) {
+                merged.add(new int[]{L, R});
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], R);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+
+    public static void main(String[] args) {
+//        int[][] result = new int[][]{{1, 4}, {4, 5}};
+//        int[][] result = new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+        int[][] result = new int[][]{{1, 4}, {0, 2}, {3, 5}};
+        System.out.println(Arrays.deepToString(merge(result)));
+    }
+}
