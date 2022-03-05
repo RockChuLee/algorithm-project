d17ac4020a9b64dc1acaddca7d699d34af379661
Hao Dong <13132193357@163.com>
2022-03-04 22:05:20 -0500

    1

--- src/main/java/com/nyu/oa/csico/CountOfBitSets.java
+++ src/main/java/com/nyu/oa/csico/CountOfBitSets.java
@@ -15,11 +15,11 @@ package com.nyu.oa.csico;
  * The binary of 18 is 10010.We calculate the number of 1's from the binary form, which is the bit set of that number.
  * The number of 1's in the binary form is 2.So,the output is 2.
  */
-public class NumberofBinary {
+public class CountOfBitSets {
     public static void main(String[] args) {
         int number = 18;
-        String s = Integer.toBinaryString(number);
         int result = 0;
+        String s = Integer.toBinaryString(number);
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '1') {
                 result++;
--- src/main/java/com/nyu/oa/csico/MeanAndMode.java
+++ src/main/java/com/nyu/oa/csico/MeanAndMode.java
@@ -1,5 +1,8 @@
 package com.nyu.oa.csico;
 
+import java.text.DecimalFormat;
+import java.util.*;
+
 /**
  * The arithmetic mean of N numbers is the sum of the numbers, divided by N.The mode of N numbers is the most frequently
  * occuring number. your program must output the mean and mode of a set of numbers.
@@ -21,7 +24,57 @@ package com.nyu.oa.csico;
  */
 
 public class MeanAndMode {
-    public static void main(String[] args) {
+    public static float[]  meanAndMode(float[] inputArr)
+    {
+        // Write your code here
+        HashMap<Float,Integer> map = new HashMap<>();
+        Integer max = 0;
+        float sum = 0;
+        ArrayList<Float> mode = new ArrayList<>();
+        DecimalFormat df = new DecimalFormat();
+        df.setMaximumFractionDigits(2);
 
+        for (float v : inputArr) {
+            map.put(v, map.getOrDefault(v, 0) + 1);
+            sum += v;
+        }
+        Set<Map.Entry<Float, Integer>> entries = map.entrySet();
+        for (Map.Entry<Float, Integer> entry : entries) {
+            Float key = entry.getKey();
+            Integer value = entry.getValue();
+            if (value > max) {
+                mode.clear();
+                mode.add(key);
+                max = value;
+            } else if (value == max) {
+                mode.add(key);
+            }
+        }
+        int size = mode.size()+1;
+        float[]  answer = new float[size];
+        answer[0] = Float.parseFloat(df.format(sum / inputArr.length));
+        for (int i = 0; i < mode.size() ; i++) {
+            answer[i + 1] = Float.parseFloat(df.format(mode.get(i)));
+        }
+        return answer;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        //input for inputArr
+        int inputArr_size = in.nextInt();
+        float inputArr[] = new float[inputArr_size];
+        for(int idx = 0; idx < inputArr_size; idx++)
+        {
+            inputArr[idx] = in.nextFloat();
+        }
+
+        float[] result = meanAndMode(inputArr);
+        for(int idx = 0; idx < result.length - 1; idx++)
+        {
+            System.out.print(result[idx] + " ");
+        }
+        System.out.print(result[result.length - 1]);
     }
 }
