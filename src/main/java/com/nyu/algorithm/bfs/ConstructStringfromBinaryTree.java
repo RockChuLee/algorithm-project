e3a943c26bb5216bbf55d5784e3a82fc58fce976
Hao Dong <13132193357@163.com>
2022-03-19 00:40:03 -0400

    1

---
+++ src/main/java/com/nyu/algorithm/bfs/ConstructStringfromBinaryTree.java
@@ -0,0 +1,77 @@
+package com.nyu.algorithm.bfs;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Set;
+
+//https://leetcode-cn.com/problems/construct-string-from-binary-tree/
+public class ConstructStringfromBinaryTree {
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    //recursion
+    public String tree2str(TreeNode root) {
+        if (root == null) {
+            return "";
+        }
+        if (root.left == null && root.right == null) {
+            return Integer.toString(root.val);
+        }
+        if (root.right == null) {
+            return root.val + "(" + tree2str(root.left) + ")";
+        }
+        return root.val + "(" + tree2str(root.left) + ")(" + tree2str(root.right) + ")";
+    }
+
+    //iterator
+    public String tree2str2(TreeNode root) {
+        StringBuilder ans = new StringBuilder();
+        Deque<TreeNode> stack = new ArrayDeque<TreeNode>();
+        stack.push(root);
+        Set<TreeNode> visited = new HashSet<TreeNode>();
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.peek();
+            if (!visited.add(node)) {
+                if (node != root) {
+                    ans.append(")");
+                }
+                stack.pop();
+            } else {
+                if (node != root) {
+                    ans.append("(");
+                }
+                ans.append(node.val);
+                if (node.left == null && node.right != null) {
+                    ans.append("()");
+                }
+                if (node.right != null) {
+                    stack.push(node.right);
+                }
+                if (node.left != null) {
+                    stack.push(node.left);
+                }
+            }
+        }
+        return ans.toString();
+    }
+}
---
+++ src/main/java/com/nyu/oa/walmart/KthLargestElementinanArray.java
@@ -0,0 +1,41 @@
+package com.nyu.oa.walmart;
+
+//https://leetcode-cn.com/problems/kth-largest-element-in-an-array/
+public class KthLargestElementinanArray {
+    public int findKthLargest(int[] nums, int k) {
+        int heapSize = nums.length;
+        buildMaxHeap(nums, heapSize);
+        for (int i = nums.length - 1; i >= nums.length - k + 1; --i) {
+            swap(nums, 0, i);
+            --heapSize;
+            maxHeapify(nums, 0, heapSize);
+        }
+        return nums[0];
+    }
+
+    public void buildMaxHeap(int[] arr, int heapSize) {
+        for (int i = heapSize / 2; i >= 0; --i) {
+            maxHeapify(arr, i, heapSize);
+        }
+    }
+
+    public void maxHeapify(int[] arr, int index, int heapSize) {
+        int l = index * 2 + 1, r = index * 2 + 2, largest = index;
+        if (l < heapSize && arr[l] > arr[largest]) {
+            largest = l;
+        }
+        if (r < heapSize && arr[r] > arr[largest]) {
+            largest = r;
+        }
+        if (largest != index) {
+            swap(arr, index, largest);
+            maxHeapify(arr, largest, heapSize);
+        }
+    }
+
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
