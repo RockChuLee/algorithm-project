bc6e70cae2075dfdfd93e11348173b8969be7c30
donghao <13132193357@163.com>
2022-03-20 17:11:22 -0400

    daily question

---
+++ src/main/java/com/nyu/algorithm/bfs/TwoSumIVInputisaBST.java
@@ -0,0 +1,144 @@
+package com.nyu.algorithm.bfs;
+
+
+import java.util.*;
+
+//https://leetcode-cn.com/problems/two-sum-iv-input-is-a-bst/
+public class TwoSumIVInputisaBST {
+
+
+    public boolean findTarget(TreeNode root, int k) {
+//        return dfs(root, k);
+//        return bfs(root, k);
+//        return dfsAndMiditeration(root, k);
+        return bfsAndMiditeration(root, k);
+    }
+
+    private boolean bfsAndMiditeration(TreeNode root, int k) {
+        TreeNode left = root, right = root;
+        Deque<TreeNode> leftStack = new ArrayDeque<TreeNode>();
+        Deque<TreeNode> rightStack = new ArrayDeque<TreeNode>();
+        leftStack.push(left);
+        while (left.left != null) {
+            leftStack.push(left.left);
+            left = left.left;
+        }
+        rightStack.push(right);
+        while (right.right != null) {
+            rightStack.push(right.right);
+            right = right.right;
+        }
+        while (left != right) {
+            if (left.val + right.val == k) {
+                return true;
+            }
+            if (left.val + right.val < k) {
+                left = getLeft(leftStack);
+            } else {
+                right = getRight(rightStack);
+            }
+        }
+        return false;
+    }
+
+    public TreeNode getLeft(Deque<TreeNode> stack) {
+        TreeNode root = stack.pop();
+        TreeNode node = root.right;
+        while (node != null) {
+            stack.push(node);
+            node = node.left;
+        }
+        return root;
+    }
+
+    public TreeNode getRight(Deque<TreeNode> stack) {
+        TreeNode root = stack.pop();
+        TreeNode node = root.left;
+        while (node != null) {
+            stack.push(node);
+            node = node.right;
+        }
+        return root;
+    }
+
+    List<Integer> list = new ArrayList<Integer>();
+
+
+    private boolean dfsAndMiditeration(TreeNode root, int k) {
+        inorderTraversal(root);
+        int left = 0, right = list.size() - 1;
+        while (left < right) {
+            if (list.get(left) + list.get(right) == k) {
+                return true;
+            }
+            if (list.get(left) + list.get(right) < k) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return false;
+    }
+
+    public void inorderTraversal(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        inorderTraversal(node.left);
+        list.add(node.val);
+        inorderTraversal(node.right);
+    }
+
+
+    private boolean bfs(TreeNode root, int k) {
+        Set<Integer> set = new HashSet<Integer>();
+        Queue<TreeNode> queue = new ArrayDeque<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (set.contains(k - node.val)) {
+                return true;
+            }
+            set.add(node.val);
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        return false;
+    }
+
+    Set<Integer> set = new HashSet<Integer>();
+
+    private boolean dfs(TreeNode root, int k) {
+        if (root == null) {
+            return false;
+        }
+        if (set.contains(k - root.val)) {
+            return true;
+        }
+        set.add(root.val);
+        return findTarget(root.left, k) || findTarget(root.right, k);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
