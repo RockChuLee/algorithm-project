9daa1ec98e1841843b9f4905e4d5f1d39cb1e952
Hao Dong <13132193357@163.com>
2022-03-27 23:01:21 -0400

    daily

--- src/main/java/com/nyu/leetcode/weekly3/KkthPalindrome.java
+++ src/main/java/com/nyu/leetcode/weekly3/KkthPalindrome.java
@@ -5,19 +5,26 @@ import java.util.List;
 
 public class KkthPalindrome {
     public long[] kthPalindrome(int[] queries, int intLength) {
-        int scale = intLength + 1 / 2;
-        for (int i = 0; i < queries.length; i++) {
-            int query = queries[i];
-            int[] operator = new int[scale];
-            for (int i1 = 0; i1 < operator.length; i1++) {
-                int value = query % 10;
-                if (i1 == 0) {
-                    value = value - 1;
-                }else {
-                    value = value + 1;
-                }
+        int lenQ = queries.length;
+        long[] res = new long[lenQ];
+        int base = (int) Math.pow(10, (intLength - 1) / 2);
+        for (int i = 0; i < lenQ; i++) {
+            int curQuery = queries[i];
+            if (curQuery > 9 * base) {
+                res[i] = -1;
+                continue;
             }
+            long x = base + curQuery - 1;
+            long y = x;
+            if (intLength % 2 == 1)
+                y = y / 10;
+
+            for (; y > 0; y /= 10) {
+                x = x * 10 + y % 10;
+            }
+            res[i] = x;
         }
-        return new long[]{};
+
+        return res;
     }
 }
