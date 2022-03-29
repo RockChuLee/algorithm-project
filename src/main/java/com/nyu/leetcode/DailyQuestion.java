eaf665464b36a048d88ad723da729bbbdcb1e8e8
Hao Dong <13132193357@163.com>
2022-03-29 18:31:14 -0400

    daily

--- src/main/java/com/nyu/leetcode/DailyQuestion.java
+++ src/main/java/com/nyu/leetcode/DailyQuestion.java
@@ -1,26 +1,42 @@
 package com.nyu.leetcode;
 
-import java.util.Arrays;
+import java.util.*;
 
-public class DailyQuestion {
-    public int[] missingRolls(int[] rolls, int mean, int n) {
-        int sum = 0;
-        for (int i = 0; i < rolls.length; i++) {
-            sum += rolls[i];
+class Solution {
+    public static List<Integer> busiestServers(int k, int[] arrival, int[] load) {
+        TreeSet<Integer> available = new TreeSet<Integer>();
+        for (int i = 0; i < k; i++) {
+            available.add(i);
         }
-        sum = (rolls.length + n) * mean - sum;
-        int[] result = new int[n];
-        if (sum > n * 6) {
-            return result;
+        PriorityQueue<int[]> busy = new PriorityQueue<int[]>((a, b) -> a[0] - b[0]);
+        int[] requests = new int[k];
+        for (int i = 0; i < arrival.length; i++) {
+            while (!busy.isEmpty() && busy.peek()[0] <= arrival[i]) {
+                available.add(busy.poll()[1]);
+            }
+            if (available.isEmpty()) {
+                continue;
+            }
+            Integer p = available.ceiling(i % k);
+            if (p == null) {
+                p = available.first();
+            }
+            requests[p]++;
+            busy.offer(new int[]{arrival[i] + load[i], p});
+            available.remove(p);
         }
-        for (int j = 1; j <= sum; j++) {
-            result[j % n]++;
+        int maxRequest = Arrays.stream(requests).max().getAsInt();
+        List<Integer> ret = new ArrayList<Integer>();
+        for (int i = 0; i < k; i++) {
+            if (requests[i] == maxRequest) {
+                ret.add(i);
+            }
         }
-        return result;
+        return ret;
     }
 
     public static void main(String[] args) {
-        DailyQuestion dailyQuestion = new DailyQuestion();
-        System.out.println(Arrays.toString(dailyQuestion.missingRolls(new int[]{3, 2, 4, 3}, 4, 2)));
+        Solution.busiestServers(3, new int[]{1, 2, 3, 4, 5}, new int[]{5, 2, 3, 3, 3});
+        //k = 3, arrival = [1,2,3,4,5], load = [5,2,3,3,3]
     }
 }
