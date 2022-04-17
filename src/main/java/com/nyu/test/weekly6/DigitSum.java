c40bad7967f62554b37dd4c62f7931ae5f1385bb
Hao Dong <13132193357@163.com>
2022-04-17 01:17:42 -0400

    weekly contest

---
+++ src/main/java/com/nyu/test/weekly6/DigitSum.java
@@ -0,0 +1,29 @@
+package com.nyu.test.weekly6;
+
+public class DigitSum {
+    static StringBuilder stringBuilder;
+
+    public static String digitSum(String s, int k) {
+        int length = s.length();
+        if (length == 0 || length <= k) {
+            return s;
+        }
+        stringBuilder = new StringBuilder();
+        int index = 0;
+        while (index < length) {
+            int sum = 0;
+            int i = index;
+            for (int j = 0; j < k && i + j < length; j++) {
+                sum += Character.getNumericValue(s.charAt(i + j));
+                index++;
+            }
+            stringBuilder.append(sum);
+        }
+        return digitSum(stringBuilder.toString(), k);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(digitSum("11111222223", 3));
+    }
+
+}
