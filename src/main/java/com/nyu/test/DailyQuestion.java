388b0b6db90f3ade4626eeb98781855d5bb507b0
Hao Dong <13132193357@163.com>
2022-05-01 00:48:53 -0400

    Daily Question

--- src/main/java/com/nyu/test/DailyQuestion.java
+++ src/main/java/com/nyu/test/DailyQuestion.java
@@ -2,52 +2,66 @@ package com.nyu.test;
 
 import java.util.*;
 
-class Solution {
-    static int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    int[][] heights;
-    int m, n;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-    public static int bfs(int[] nums, int k, int p) {
-        int length = nums.length;
-        StringBuilder stringBuilder;
-        HashSet<String> result = new HashSet<>();
-        for (int i = 0; i < length; i++) {
-            stringBuilder = new StringBuilder();
-            int temp = nums[i] % p == 0 ? 1 : 0;
-            stringBuilder.append(nums[i]).append(",");
-            System.out.println(stringBuilder.toString());
-            System.out.println(result.add(stringBuilder.toString()));
-            for (int j = i + 1; j < length; j++) {
-                if (nums[j] % p == 0) {
-                    temp++;
-                }
-                if (temp > k) {
-                    break;
-                }
-                stringBuilder.append(nums[j]).append(",");
-                System.out.println(stringBuilder.toString());
-                System.out.println(result.add(stringBuilder.toString()));
-            }
-        }
-        return result.size();
+    TreeNode() {
     }
 
-    public static int countDistinct(int[] nums, int k, int p) {
-        HashSet<String> set = new HashSet<>();
-        for (int i = 0, count = 0; i < nums.length; i++, count = 0) {
-            ArrayList<Integer> list = new ArrayList<>();
-            for (int j = i; j < nums.length; j++) {
-                list.add(nums[j]);
-                if ((count += nums[j] % p > 0 ? 0 : 1) <= k) {
-                    System.out.println(String.valueOf(list));
-                    set.add(String.valueOf(list));
-                }
-            }
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        List<Integer> result = new ArrayList<>();
+        dfs(root1, heap);
+        dfs(root2, heap);
+
+        while (heap.peek()!=null) {
+            result.add(heap.poll());
         }
-        return set.size();
+
+        return result;
+    }
+
+    private void dfs(TreeNode root, PriorityQueue<Integer> heap) {
+        if (root != null) {
+            heap.offer(root.val);
+        }else {
+            return;
+        }
+        if (root.left != null) {
+            dfs(root.left, heap);
+
+        }
+        if (root.right != null) {
+            dfs(root.right, heap);
+
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println(bfs(new int[]{5, 11, 17, 13, 16, 9, 4, 9, 20}, 7, 1));
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        heap.offer(3);
+        heap.offer(1);
+        heap.offer(2);
+        heap.offer(6);
+        heap.offer(4);
+        System.out.println(heap.poll());
+        System.out.println(heap.poll());
+        System.out.println(heap.poll());
+        System.out.println(heap.poll());
+        System.out.println(heap.poll());
     }
 }
