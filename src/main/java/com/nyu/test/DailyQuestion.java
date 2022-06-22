d4a17b55901862b05f2643bc80bca20c96af999e
Hao Dong <13132193357@163.com>
2022-06-22 13:11:11 -0400

    Weekly contest

--- src/main/java/com/nyu/test/DailyQuestion.java
+++ src/main/java/com/nyu/test/DailyQuestion.java
@@ -2,6 +2,7 @@ package com.nyu.test;
 
 import com.nyu.util.In;
 
+import java.security.Key;
 import java.text.DecimalFormat;
 import java.util.*;
 import java.util.regex.Matcher;
@@ -10,48 +11,28 @@ import java.util.stream.Collectors;
 
 class Solution {
 
-    public static int totalSteps(int[] nums) {
-//        ArrayList<Integer> temp = new ArrayList<>();
-        if (nums.length == 0 || nums.length == 1) {
+    public static int longestSubsequence(String s, int k) {
+        String binaryString = Integer.toBinaryString(k);
+        if (binaryString.length() > s.length()) {
             return 0;
         }
-        ArrayList<Integer> num = (ArrayList<Integer>) Arrays.stream(nums).boxed().collect(Collectors.toList());
-        boolean flag = true;
-        boolean first = true;
         int result = 0;
-        int i = 1;
-        int size = num.size();
-        int temp = num.get(0);
-        while (flag) {
-            if (first) {
-                size = num.size();
-                temp = num.get(0);
-            }
-            if(size == 1){
-                return result;
-            }
-            first = false;
-            if (temp > num.get(i)) {
-                temp = num.get(i);
-                num.remove(i);
-            } else {
-                temp = num.get(i);
-                i++;
-            }
-
-            if (i == num.size()) {
-                if (size == num.size()) {
-                    flag = false;
-                }
-                i = 1;
-                result++;
-                first = true;
+        int index = 0;
+        for (int i = s.length() - binaryString.length(); i < s.length(); i++) {
+            if (String.valueOf(s.charAt(i)).equals("1")) {
+                index = i;
+                break;
             }
         }
-        return result;
+        for (int i = 0; i < index ; i++) {
+            if (String.valueOf(s.charAt(i)).equals("0")) {
+                result++;
+            }
+        }
+        return result + s.length() - index;
     }
 
     public static void main(String[] args) {
-        System.out.println(totalSteps(new int[]{5, 11, 7, 8, 11}));
+        System.out.println(longestSubsequence("1001010", 5));
     }
 }
