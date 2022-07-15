9054adae9643de3293818a7e14fbc485c065a865
Hao Dong <13132193357@163.com>
2022-07-14 23:54:06 -0400

    DailyQuestion

--- src/main/java/com/nyu/test/DailyQuestion.java
+++ src/main/java/com/nyu/test/DailyQuestion.java
@@ -1,38 +1,35 @@
 package com.nyu.test;
 
-import com.nyu.util.In;
-
-import java.security.Key;
-import java.text.DecimalFormat;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 class Solution {
 
-    public static int longestSubsequence(String s, int k) {
-        String binaryString = Integer.toBinaryString(k);
-        if (binaryString.length() > s.length()) {
-            return 0;
-        }
-        int result = 0;
-        int index = 0;
-        for (int i = s.length() - binaryString.length(); i < s.length(); i++) {
-            if (String.valueOf(s.charAt(i)).equals("1")) {
-                index = i;
-                break;
-            }
-        }
-        for (int i = 0; i < index ; i++) {
-            if (String.valueOf(s.charAt(i)).equals("0")) {
-                result++;
-            }
-        }
-        return result + s.length() - index;
+    Stack<Integer> in;
+    Stack<Integer> out;
+    public Solution() {
+        in = new Stack<>();
+        out = new Stack<>();
     }
 
     public static void main(String[] args) {
-        System.out.println(longestSubsequence("1001010", 5));
+        Solution obj = new Solution();
+        obj.appendTail(1);
+        int param_2 = obj.deleteHead();
+    }
+
+    public void appendTail(int value) {
+        in.push(value);
+    }
+
+    public int deleteHead() {
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
+            }
+        }
+        return out.isEmpty() ? -1 : out.pop();
     }
 }
