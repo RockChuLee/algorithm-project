18a95dfeb3df0b5dc2b4d79a10f4188697194226
Hao Dong <13132193357@163.com>
2022-07-26 16:38:25 -0400

    1

--- src/main/java/com/nyu/test/DailyQuestion.java
+++ src/main/java/com/nyu/test/DailyQuestion.java
@@ -1,31 +1,44 @@
 package com.nyu.test;
 
-import java.util.*;
-
 class Solution {
-    public static char firstUniqChar(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
-        }
-        char ans = ' ';
-        for (char key : map.keySet()) {
-            if (map.get(key) == 1) {
-                ans = key;
-                break;
+    public static int[][] candyCrush(int[][] board) {
+        int R = board.length, C = board[0].length;
+
+        boolean todo = false;
+
+        for (int r = 0; r < R; ++r) {
+            for (int c = 0; c + 2 < C; ++c) {
+                int v = Math.abs(board[r][c]);
+                if (v != 0 && v == Math.abs(board[r][c + 1]) && v == Math.abs(board[r][c + 2])) {
+                    board[r][c] = board[r][c + 1] = board[r][c + 2] = -v;
+                    todo = true;
+                }
             }
         }
-        List<Character> exist = new ArrayList<>();
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.isEmpty();
-        queue.poll();
-        return ans;
+
+        for (int r = 0; r + 2 < R; ++r) {
+            for (int c = 0; c < C; ++c) {
+                int v = Math.abs(board[r][c]);
+                if (v != 0 && v == Math.abs(board[r + 1][c]) && v == Math.abs(board[r + 2][c])) {
+                    board[r][c] = board[r + 1][c] = board[r + 2][c] = -v;
+                    todo = true;
+                }
+            }
+        }
+
+        for (int c = 0; c < C; ++c) {
+            int wr = R - 1;
+            for (int r = R - 1; r >= 0; --r)
+                if (board[r][c] > 0)
+                    board[wr--][c] = board[r][c];
+            while (wr >= 0)
+                board[wr--][c] = 0;
+        }
+
+        return todo ? candyCrush(board) : board;
     }
 
     public static void main(String[] args) {
-        firstUniqChar("leetcode");
-        int mod = 10000007;
-        System.out.println((10000008 + 10000008) % mod);
-        System.out.println((10000008 + 10000008) % mod);
+        candyCrush(new int[][]{{110, 5, 112, 113, 114}, {210, 211, 5, 213, 214}, {310, 311, 3, 313, 314}, {410, 411, 412, 5, 414}, {5, 1, 512, 3, 3}, {610, 4, 1, 613, 614}, {710, 1, 2, 713, 714}, {810, 1, 2, 1, 1}, {1, 1, 2, 2, 2}, {4, 1, 4, 4, 1014}});
     }
 }
