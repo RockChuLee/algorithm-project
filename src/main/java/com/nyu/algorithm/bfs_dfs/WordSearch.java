0e39631a472513135d4ea5cefa96d7cd6131ed11
Hao Dong <13132193357@163.com>
2022-07-28 00:45:35 -0400

    1

---
+++ src/main/java/com/nyu/algorithm/bfs_dfs/WordSearch.java
@@ -0,0 +1,6 @@
+package com.nyu.algorithm.bfs_dfs;
+
+//https://leetcode.cn/problems/word-search/
+public class WordSearch {
+
+}
--- src/main/java/com/nyu/test/DailyQuestion.java
+++ src/main/java/com/nyu/test/DailyQuestion.java
@@ -1,44 +1,53 @@
 package com.nyu.test;
 
+import com.nyu.util.std.StdOut;
+
 class Solution {
-    public static int[][] candyCrush(int[][] board) {
-        int R = board.length, C = board[0].length;
+    static int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    static boolean[][] visited;
+    static int count = 0;
 
-        boolean todo = false;
+    public static int movingCount(int m, int n, int k) {
+        if(m == 0|| n ==0){
+            return 0;
+        }
 
-        for (int r = 0; r < R; ++r) {
-            for (int c = 0; c + 2 < C; ++c) {
-                int v = Math.abs(board[r][c]);
-                if (v != 0 && v == Math.abs(board[r][c + 1]) && v == Math.abs(board[r][c + 2])) {
-                    board[r][c] = board[r][c + 1] = board[r][c + 2] = -v;
-                    todo = true;
-                }
+        visited = new boolean[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                dfs(i,j,m,n,k,visited);
             }
         }
+        return count;
+    }
 
-        for (int r = 0; r + 2 < R; ++r) {
-            for (int c = 0; c < C; ++c) {
-                int v = Math.abs(board[r][c]);
-                if (v != 0 && v == Math.abs(board[r + 1][c]) && v == Math.abs(board[r + 2][c])) {
-                    board[r][c] = board[r + 1][c] = board[r + 2][c] = -v;
-                    todo = true;
-                }
+    public static void dfs(int i, int j,int m, int n, int k, boolean[][] visited){
+        if(sum(i)+sum(j)<=k&&!visited[i][j]){
+            count++;
+        }else{
+            return;
+        }
+        visited[i][j] = true;
+
+        for (int[] dir : dirs) {
+            int newi = i + dir[0];
+            int newj = j + dir[1];
+            if(newi >= 0 && newi < m && newj >= 0 && newj < n){
+                dfs(newi,newj,m,n,k,visited);
             }
         }
+    }
 
-        for (int c = 0; c < C; ++c) {
-            int wr = R - 1;
-            for (int r = R - 1; r >= 0; --r)
-                if (board[r][c] > 0)
-                    board[wr--][c] = board[r][c];
-            while (wr >= 0)
-                board[wr--][c] = 0;
+    public static int sum(int i){
+        int sum = 0;
+        while(i>10){
+            sum+=i%10;
+            i = i/10;
         }
-
-        return todo ? candyCrush(board) : board;
+        return sum+i;
     }
 
     public static void main(String[] args) {
-        candyCrush(new int[][]{{110, 5, 112, 113, 114}, {210, 211, 5, 213, 214}, {310, 311, 3, 313, 314}, {410, 411, 412, 5, 414}, {5, 1, 512, 3, 3}, {610, 4, 1, 613, 614}, {710, 1, 2, 713, 714}, {810, 1, 2, 1, 1}, {1, 1, 2, 2, 2}, {4, 1, 4, 4, 1014}});
+        System.out.println(movingCount(16, 8, 4));
     }
 }
