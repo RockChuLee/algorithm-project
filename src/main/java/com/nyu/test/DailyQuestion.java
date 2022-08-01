de0d52c10c9e52345f29c7c402a20df1e60714e9
Hao Dong <13132193357@163.com>
2022-08-01 02:02:30 -0400

    1

--- src/main/java/com/nyu/test/DailyQuestion.java
+++ src/main/java/com/nyu/test/DailyQuestion.java
@@ -1,4 +1,20 @@
 package com.nyu.test;
 
 class Solution {
+    public int longestCycle(int[] edges) {
+        int n = edges.length, ans = -1;
+        int[] time = new int[n];
+        for (int i = 0, clock = 1; i < n; ++i) {
+            if (time[i] > 0) continue;
+            for (int x = i, start_time = clock; x >= 0; x = edges[x]) {
+                if (time[x] > 0) { // 重复访问
+                    if (time[x] >= start_time) // 找到了一个新的环
+                        ans = Math.max(ans, clock - time[x]);
+                    break;
+                }
+                time[x] = clock++;
+            }
+        }
+        return ans;
+    }
 }
