52b257bb999fed67b5b737546d29c184b19f4fd4
Hao Dong <13132193357@163.com>
2022-08-02 23:52:04 -0400

    1

--- src/main/java/com/nyu/test/DailyQuestion.java
+++ src/main/java/com/nyu/test/DailyQuestion.java
@@ -1,20 +1,20 @@
 package com.nyu.test;
 
 class Solution {
-    public int longestCycle(int[] edges) {
-        int n = edges.length, ans = -1;
-        int[] time = new int[n];
-        for (int i = 0, clock = 1; i < n; ++i) {
-            if (time[i] > 0) continue;
-            for (int x = i, start_time = clock; x >= 0; x = edges[x]) {
-                if (time[x] > 0) { // 重复访问
-                    if (time[x] >= start_time) // 找到了一个新的环
-                        ans = Math.max(ans, clock - time[x]);
-                    break;
-                }
-                time[x] = clock++;
-            }
+    public static void main(String[] args) {
+        System.out.println(myPow(2.0,-2147483648));
+    }
+
+    public static double myPow(double x, int n) {
+        if(n<0){
+            x = 1/x;
+            n = -n;
         }
-        return ans;
+        if(n==0){
+            return 1;
+        }
+        // System.out.println(n);
+        double ans = myPow(x,n/2);
+        return n%2==0?ans*ans:ans*ans*x;
     }
 }
