7e83de8593d153da48f11865dc42c17f7d61f734
Hao Dong <13132193357@163.com>
2022-09-01 20:50:01 -0400

    1

--- src/main/java/com/nyu/oa/optiver/IsATree.java
+++ src/main/java/com/nyu/oa/optiver/IsATree.java
@@ -5,10 +5,10 @@ import java.util.HashSet;
 public class IsATree {
 
     public static void main(String[] args) {
-//        String s = "(A,B) (B,D) (D,E) (A,C) (C,F) (E,G)";
+        String s = "(A,B) (B,D) (D,E) (A,C) (C,F) (E,G)";
 //        String s = "(A,B) (A,C) (B,D) (D,E) (C,F) (E,G)";
 //        String s = "(A,B) (A,C) (B,D) (D,C)";
-        String s = "(A,B) (A,C) (X,Y) (X,Z)";
+//        String s = "(A,B) (A,C) (X,Y) (X,Z)";
 //        String s = "asafdsafasdf";
         System.out.println(GetSExpression(s));
     }
@@ -20,6 +20,18 @@ public class IsATree {
     //E5 lnput Contains Cycle
 
     public static String GetSExpression(String s) {
+        //check error E1: lnvalid lnput Format
+        String[] s1 = s.split(" ");
+        if (s1.length == 0) {
+            return "E1";
+        } else {
+            for (String s2 : s1) {
+                if (!(s2.charAt(0) == '('&&Character.isLetter(s2.charAt(1))&&s2.charAt(2) == ','&&Character.isLetter(s2.charAt(3))&&s2.charAt(4) == ')')) {
+                    return "E1";
+                }
+            }
+        }
+
         boolean[][] graph = new boolean[26][26];
         HashSet<Character> nodes = new HashSet<Character>();
         //construct graph and check error E2: duplicate edges
@@ -32,8 +44,7 @@ public class IsATree {
             nodes.add(s.charAt(i));
             nodes.add(s.charAt(i + 2));
         }
-        //check error E1: more than 2 children
-        boolean E1 = false;
+        //check error E3 Parent Has More than Two Children
         for (int i = 0; i < 26; i++) {
             int count = 0; //number of child
             for (int j = 0; j < 26; j++) {
@@ -41,7 +52,7 @@ public class IsATree {
                     count++;
             }
             if (count > 2)
-                return "E1";
+                return "E3";
         }
         if (E2) return "E2"; //return E2 after checking E1
 
@@ -57,13 +68,13 @@ public class IsATree {
                     root = node;
                     boolean[] visited = new boolean[26];
                     if (IsCycle(node, graph, visited))
-                        return "E3";
+                        return "E5";
                 }
             }
         }
-        if (numOfRoots == 0) return "E3"; //if no root, must be PivotIndex cycle
+        if (numOfRoots == 0) return "E1"; //if no root, must be PivotIndex cycle
         if (numOfRoots > 1) return "E4"; //if more than one roots
-        if (root == ' ') return "E5"; //if no edge in input string, invalid input error
+        if (root == ' ') return "E1"; //if no edge in input string, invalid input error
         return GetExpressionHelper(root, graph);
 
     }
--- src/main/java/com/nyu/oa/optiver/NumberofDaysBetweenTwoDates.java
+++ src/main/java/com/nyu/oa/optiver/NumberofDaysBetweenTwoDates.java
@@ -6,15 +6,12 @@ public class NumberofDaysBetweenTwoDates {
     int[] days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     public int daysBetweenDates(String date1, String date2) {
-        return Math.abs(countDays(getDate(date2)) - countDays(getDate(date1)));
+        int[] dates1 = getDate(date1), dates2 = getDate(date2);
+        return Math.abs(countDays(dates1[0], dates1[1],dates1[2]) - countDays(dates2[0], dates2[1],dates2[2]));
     }
 
-    private int[] getDate(String d) {
-        return Arrays.stream(d.split("-")).mapToInt(p -> Integer.valueOf(p)).toArray();
-    }
-
-    private int countDays(int[] d) {
-        return countDays(d[0], d[1], d[2]);
+    private int[] getDate(String date) {
+            return new int[]{Integer.parseInt(date.substring(0, 4)), Integer.parseInt(date.substring(5, 7)), Integer.parseInt(date.substring(8, 10))};
     }
 
     private int countDays(int y, int m, int d) {
