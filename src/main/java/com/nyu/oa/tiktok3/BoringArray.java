d70aae4e0fcb34283a004134391074aee59d15d9
Hao Dong <13132193357@163.com>
2022-10-06 21:17:40 -0400

    1

--- src/main/java/com/nyu/oa/tiktok3/BoringArray.java
+++ src/main/java/com/nyu/oa/tiktok3/BoringArray.java
@@ -1,5 +1,7 @@
 package com.nyu.oa.tiktok3;
 
+import java.util.Arrays;
+
 public class BoringArray {
     public static boolean transformArray(String A, String B) {
         int N = 10;
@@ -23,7 +25,29 @@ public class BoringArray {
         return true;
     }
 
+    public static boolean transformArray1(String A, String B) {
+        String[] Astr = A.split(" ");
+        int[] As = new int[Astr.length];
+        for (int i = 0; i < Astr.length; i++) {
+            As[i] = Integer.parseInt(Astr[i]);
+        }
+        Arrays.sort(As);
+        String[] Bstr = B.split(" ");
+        int[] Bs = new int[Astr.length];
+        for (int i = 0; i < Bstr.length; i++) {
+            Bs[i] = Integer.parseInt(Bstr[i]);
+        }
+        Arrays.sort(Bs);
+        for (int i = 0; i < As.length; i++) {
+            if (As[i] != Bs[i] && Bs[i] - As[i] != 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(transformArray("-10 2 3 4 9", "-10 2 3 4 10"));
+        System.out.println(transformArray1("-10 2 3 4 9", "-10 2 3 4 10"));
     }
 }
