b51a1958e7622b85ddb1761672d1b9cdddfe15eb
Hao Dong <13132193357@163.com>
2022-10-07 20:33:46 -0400

    1

--- src/main/java/com/nyu/oa/expedia/ArrayGenerator.java
+++ src/main/java/com/nyu/oa/expedia/ArrayGenerator.java
@@ -1,11 +1,22 @@
 package com.nyu.oa.expedia;
 
-import java.util.HashMap;
-import java.util.List;
-
 public class ArrayGenerator {
     public static int[] getSmallestArray(int[] arr,int r, int l) {
-
-        return new int[]{};
+        int length = arr.length;
+        if (length == 0) {
+            return new int[]{};
+        }
+        int[] brr = new int[length];
+        int[] diff = new int[length];
+        brr[0] = Math.max(arr[0], r);
+        diff[0] = 0;
+        for (int i = 1; i < length; i++) {
+            brr[i] = Math.max(brr[i - 1], arr[i] + diff[i - 1]);
+            if (brr[i] > l) {
+                return new int[]{-1};
+            }
+            diff[i] = brr[i] - arr[i];
+        }
+        return brr;
     }
 }
