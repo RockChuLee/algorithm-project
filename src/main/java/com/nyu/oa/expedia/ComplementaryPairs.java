facf4307f2824fe4f6bef482fd2f9bdc407ad9de
Hao Dong <13132193357@163.com>
2022-10-07 20:08:31 -0400

    1

--- src/main/java/com/nyu/oa/expedia/ComplementaryPairs.java
+++ src/main/java/com/nyu/oa/expedia/ComplementaryPairs.java
@@ -1,5 +1,6 @@
 package com.nyu.oa.expedia;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -8,14 +9,30 @@ public class ComplementaryPairs {
     public static long countComplementary(List<String> lists, int size) {
         long ans = 0;
         HashMap<Long, Long> map = new HashMap<>();
-        for (int i = 0; i < lists.size(); i++) {
-            String cur = lists.get(i);
+        for (String cur : lists) {
             long temp = 0;
             for (int j = 0; j < cur.length(); j++) {
-                temp ^= (1 << (int) cur.charAt(j));
+                temp ^= (1L << (int) cur.charAt(j) - 'a');
             }
-            ans += map.getOrDefault(temp, 0L);
+            Long aLong = map.getOrDefault(temp, 0L);
+            ans += aLong;
+            for (int j = 0; j < 26; j++) {
+                long bitmask = temp ^ (1L << j);;
+                aLong = map.getOrDefault(bitmask, 0L);
+                ans += aLong;
+            }
+            map.put(temp, aLong + 1);
         }
         return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countComplementary(Arrays.asList("abc", "abcd", "bc", "adc"), 4));
+//        String cur = "bc";
+//        long temp = 0;
+//        for (int j = 0; j < cur.length(); j++) {
+//            temp ^= (1L << (int) cur.charAt(j) - 'a');
+//        }
+//        System.out.println(temp);
     }
 }
