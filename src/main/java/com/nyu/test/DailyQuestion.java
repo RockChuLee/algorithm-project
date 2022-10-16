b1458e589f4db30566ddaa7eb8bf33185a509845
Hao Dong <13132193357@163.com>
2022-10-16 00:27:37 -0400

    1

--- src/main/java/com/nyu/test/DailyQuestion.java
+++ src/main/java/com/nyu/test/DailyQuestion.java
@@ -1,36 +1,20 @@
 package com.nyu.test;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class DailyQuestion {
-
-    public static List<Integer> findKthNextGreaterElement(List<Integer> prices, int k) {
-        List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < prices.size(); i++) {
-            int curr = prices.get(i);
-            int index = 0;
-            boolean flag = true;
-            for (int j = i + 1; j < prices.size(); j++) {
-                if (prices.get(j) > curr) {
-                    index++;
-                    if (index == k) {
-                        result.add(j + 1);
-                        flag = false;
-                        break;
-                    }
-                }
-            }
-            if (flag) {
-                result.add(-1);
-            }
+    public long countSubarrays(int[] nums, int minK, int maxK) {
+        Long ans = 0L;
+        int n = nums.length, minI = -1, maxI = -1, i0 = -1;
+        for (int i = 0; i < n; ++i) {
+            int x = nums[i];
+            if (x == minK) minI = i;
+            if (x == maxK) maxI = i;
+            if (x < minK || x > maxK) i0 = i; // 子数组不能包含 nums[i0]
+            ans += Math.max(Math.min(minI, maxI) - i0, 0);
         }
-        return result;
+        return ans;
     }
 
     public static void main(String[] args) {
-        System.out.println(findKthNextGreaterElement(Arrays.asList(1, 4, 2, 5, 3), 2));
     }
 }
