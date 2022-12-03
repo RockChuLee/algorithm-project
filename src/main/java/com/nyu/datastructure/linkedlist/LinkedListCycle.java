1a6f4817a7d6269eacf266becea7aee354fc2e3c
Hao Dong <13132193357@163.com>
2022-12-02 23:04:35 -0500

    1

--- src/main/java/com/nyu/datastructure/linkedlist/LinkedListCycle.java
+++ src/main/java/com/nyu/datastructure/linkedlist/LinkedListCycle.java
@@ -26,37 +26,15 @@ public class LinkedListCycle {
     // Time Complexity : N
     // Space Complexity : 1
     public boolean hasCycle1(ListNode head) {
-        if (head == null || head.next == null) {
-            return false;
-        }
-
-        //快指针在慢指针的下一个点， 用while
-
-//        ListNode slow = head;
-//        ListNode fast = head;
-//        do {
-//            if (fast == null || fast.next == null) {
-//                return false;
-//            }
-//            slow = slow.next;
-//            fast = fast.next.next;
-//        } while (slow != fast);
-
-        //快慢指针在同一个点，用do while
-        
         ListNode slow = head;
-        ListNode fast = head.next;
-        while (slow != fast) {
-            if (fast == null || fast.next == null) {
-                return false;
-            }
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
         }
-        return true;
-    }
-
-
-    public LinkedListCycle() {
+        return false;
     }
 }
