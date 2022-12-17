93389bd2f6890fa4872c83afe24287dbe26b9e10
Hao Dong <13132193357@163.com>
2022-12-17 00:26:33 -0500

    1

--- src/main/java/com/nyu/test/DailyQuestion.java
+++ src/main/java/com/nyu/test/DailyQuestion.java
@@ -2,39 +2,31 @@ package com.nyu.test;
 
 
 public class DailyQuestion {
-    public static int minSubArrayLen(int target, int[] nums) {
-        int minLength = Integer.MAX_VALUE;
-        int left = 0;
-        int right = 0;
-        int sum = 0;
-        while (right != nums.length || sum >= target) {
-            if (sum < target) {
-                sum += nums[right++];
-//                System.out.println(1 + ":" + sum);
-            } else {
-                if (right - left <= minLength) {
-                    minLength = right - left;
-                }
-                sum -= nums[left++];
-//                System.out.println(2 + ":" + sum);
-            }
-        }
-        return minLength == Integer.MAX_VALUE ? 0 : minLength;
-    }
 
-    public static int maxRepeating(String sequence, String word) {
-        int count = 0;
-        for (int i = 0; i <= sequence.length() - word.length(); i++) {
-            if (sequence.substring(i, i + word.length()).equals(word)) {
-                count++;
-                i+=word.length()-1;
+    public boolean canChoose(int[][] groups, int[] nums) {
+        int n = 0;
+        int g = 0;
+        while (g < groups.length && n < nums.length) {
+            int[] group = groups[g];
+            int index = 0;
+            if (nums[n] == group[index]) {
+                index++;
+                if (index == group.length) {
+                    g++;
+                    index = 0;
+                    break;
+                }
+            } else {
+                index = 0;
             }
+            n++;
+
         }
-        return count;
+        return g == groups.length;
     }
 
     public static void main(String[] args) {
 //        System.out.println(minSubArrayLen(7, new int[]{2, 3, 1, 2, 4, 3, 2, 3, 1, 2, 4, 3, 99, 100, 2, 3, 1, 2, 4, 3}));
-        System.out.println(maxRepeating("aaabaaaabaaabaaaabaaaabaaaabaaaaba", "aaaba"));
+//        System.out.println(maxRepeating("aaabaaaabaaabaaaabaaaabaaaabaaaaba", "aaaba"));
     }
 }
