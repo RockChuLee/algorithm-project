f1919d13b228b0d56a8e11b20d597fdfcddd48a9
Hao Dong <13132193357@163.com>
2023-01-14 23:11:14 -0500

    1

--- src/main/java/com/nyu/test/DailyQuestion.java
+++ src/main/java/com/nyu/test/DailyQuestion.java
@@ -1,42 +1,69 @@
 package com.nyu.test;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class DailyQuestion {
 
-    public static boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int up = 0;
-        int left = 0;
-        int down = m-1;
-        int right = n-1;
-        int row = -1;
-        while(up<=down){
-            int mid = up+(down-up)/2;
-            if(matrix[mid][0]<target){
-                up = mid+1;
-            }else if(matrix[mid][0]>target){
-                down = mid-1;
-            }else if(matrix[mid][0]==target){
-                return true;
+    public static long countGood(int[] nums, int k) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int left = 0, right = 0;
+        long res = 0;
+        long count = 0;
+        while (right < nums.length && left <= right) {
+            if (count >= k) {
+                res += nums.length - right + 1;
+                List<Integer> temp = map.get(nums[left]);
+                int n = temp.size();
+                count -= ((long) n * (n - 1)) / 2;
+                temp.remove((Integer) left);
+                n = temp.size();
+                count += ((long) n * (n - 1)) / 2;
+                left++;
+            } else {
+                List<Integer> temp = new ArrayList<>();
+                if (map.containsKey(nums[right])) {
+                    temp = map.get(nums[right]);
+                    int n = temp.size();
+                    count -= ((long) n * (n - 1)) / 2;
+                }
+                temp.add(right);
+                map.put(nums[right], temp);
+                int n = temp.size();
+                count += ((long) n * (n - 1)) / 2;
+                right++;
             }
         }
-        row = matrix[up][0]<target?up:up-1;
+        while (left != right) {
+            if (count(map) >= k) {
+                res++;
+                List<Integer> temp = map.get(nums[left]);
+                temp.remove((Integer) left);
+                left++;
+            } else {
+                break;
+            }
+        }
+        return res;
 
-        while(left<=right){
-            int mid = left+(right-left)/2;
-            if(matrix[row][mid]<target){
-                left = mid+1;
-            }else if(matrix[row][mid]>target){
-                right = mid-1;
-            }else if(matrix[row][mid]==target){
-                return true;
-            }
+    }
+
+    public static long count(Map<Integer, List<Integer>> map) {
+        if (map.size() == 0) {
+            return 0;
         }
-        return false;
+        long sum = 0;
+        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+            int n = entry.getValue().size();
+            sum += ((long) n * (n - 1)) / 2;
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
-        searchMatrix(new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,60}},3);
+        System.out.println(countGood(new int[]{3, 1, 4, 3, 2, 2, 4}, 2));
     }
 }
