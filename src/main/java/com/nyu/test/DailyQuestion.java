8a6d86ad2a3ef1089b8fc1e11ca508182c648157
Hao Dong <13132193357@163.com>
2023-01-28 01:37:49 -0500

    1

--- src/main/java/com/nyu/test/DailyQuestion.java
+++ src/main/java/com/nyu/test/DailyQuestion.java
@@ -1,69 +1,35 @@
 package com.nyu.test;
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class DailyQuestion {
-
-    public static long countGood(int[] nums, int k) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        int left = 0, right = 0;
-        long res = 0;
-        long count = 0;
-        while (right < nums.length && left <= right) {
-            if (count >= k) {
-                res += nums.length - right + 1;
-                List<Integer> temp = map.get(nums[left]);
-                int n = temp.size();
-                count -= ((long) n * (n - 1)) / 2;
-                temp.remove((Integer) left);
-                n = temp.size();
-                count += ((long) n * (n - 1)) / 2;
-                left++;
+    public static int waysToMakeFair(int[] nums) {
+        int size = nums.length;
+        int[] oddSums = new int[size / 2 + 1];
+        int[] evenSums = new int[size / 2 + 1];
+        int oddSum = 0, evenSum = 0;
+        boolean flag = true;
+        for (int i = 0; i < size; i++) {
+            if (flag) {
+                evenSum += nums[i];
+                evenSums[i / 2] = evenSum;
             } else {
-                List<Integer> temp = new ArrayList<>();
-                if (map.containsKey(nums[right])) {
-                    temp = map.get(nums[right]);
-                    int n = temp.size();
-                    count -= ((long) n * (n - 1)) / 2;
-                }
-                temp.add(right);
-                map.put(nums[right], temp);
-                int n = temp.size();
-                count += ((long) n * (n - 1)) / 2;
-                right++;
+                oddSum += nums[i];
+                oddSums[i / 2] = oddSum;
+            }
+            flag = !flag;
+        }
+        int result = 0;
+        for (int i = 0; i < size; i++) {
+            int i1 = evenSums[i / 2] + (oddSums[size / 2 ] - oddSums[i / 2 + 1]);
+            int i2 = oddSums[i / 2] + (evenSums[size / 2 ] - evenSums[i / 2 + 1]);
+            if (i1 == i2) {
+                result++;
             }
         }
-        while (left != right) {
-            if (count(map) >= k) {
-                res++;
-                List<Integer> temp = map.get(nums[left]);
-                temp.remove((Integer) left);
-                left++;
-            } else {
-                break;
-            }
-        }
-        return res;
-
-    }
-
-    public static long count(Map<Integer, List<Integer>> map) {
-        if (map.size() == 0) {
-            return 0;
-        }
-        long sum = 0;
-        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
-            int n = entry.getValue().size();
-            sum += ((long) n * (n - 1)) / 2;
-        }
-        return sum;
+        return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(countGood(new int[]{3, 1, 4, 3, 2, 2, 4}, 2));
+        System.out.println(waysToMakeFair(new int[]{2, 1, 6, 4}));
     }
 }
