3cd5507804e21c577285c5f2f226744216b280f0
Hao Dong <13132193357@163.com>
2023-01-28 23:15:09 -0500

    1

--- src/main/java/com/nyu/test/DailyQuestion.java
+++ src/main/java/com/nyu/test/DailyQuestion.java
@@ -1,35 +1,27 @@
 package com.nyu.test;
 
 
-public class DailyQuestion {
-    public static int waysToMakeFair(int[] nums) {
-        int size = nums.length;
-        int[] oddSums = new int[size / 2 + 1];
-        int[] evenSums = new int[size / 2 + 1];
-        int oddSum = 0, evenSum = 0;
-        boolean flag = true;
-        for (int i = 0; i < size; i++) {
-            if (flag) {
-                evenSum += nums[i];
-                evenSums[i / 2] = evenSum;
-            } else {
-                oddSum += nums[i];
-                oddSums[i / 2] = oddSum;
-            }
-            flag = !flag;
-        }
-        int result = 0;
-        for (int i = 0; i < size; i++) {
-            int i1 = evenSums[i / 2] + (oddSums[size / 2 ] - oddSums[i / 2 + 1]);
-            int i2 = oddSums[i / 2] + (evenSums[size / 2 ] - evenSums[i / 2 + 1]);
-            if (i1 == i2) {
-                result++;
-            }
-        }
-        return result;
-    }
+import java.util.Arrays;
 
-    public static void main(String[] args) {
-        System.out.println(waysToMakeFair(new int[]{2, 1, 6, 4}));
+public class DailyQuestion {
+    public long putMarbles(int[] weights, int k) {
+        int n = weights.length;
+        long[][] dp = new long[n + 1][k + 1];
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(dp[i], Integer.MIN_VALUE);
+        }
+        dp[0][0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= k; j++) {
+                for (int l = 0; l < i; l++) {
+                    dp[i][j] = Math.max(dp[i][j], dp[l][j - 1] + (long) weights[l] * (long) weights[i - 1]);
+                }
+            }
+        }
+        long res = 0;
+        for (int i = 0; i <= k; i++) {
+            res = Math.max(res, dp[n][i]);
+        }
+        return res;
     }
 }
