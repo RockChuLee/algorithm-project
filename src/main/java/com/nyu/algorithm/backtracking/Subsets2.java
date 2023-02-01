7c5a9c62f134495f46f126216160ec74d93136f8
Hao Dong <13132193357@163.com>
2023-01-31 23:45:00 -0500

    1

---
+++ src/main/java/com/nyu/algorithm/backtracking/Subsets2.java
@@ -0,0 +1,31 @@
+package com.nyu.algorithm.backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Subsets2 {
+    // Similar to subsets 1. Here, we'll just take care of the duplicates.
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        subSet(nums, 0, ans, list);
+        return ans;
+    }
+
+    public void subSet(int[] nums, int idx, List<List<Integer>> ans, List<Integer> list) {
+        ans.add(new ArrayList<>(list));
+
+        for (int i = idx; i < nums.length; i++) {
+            //skip the duplicate elements
+            if (i > idx && nums[i] == nums[i - 1]) continue;
+            list.add(nums[i]);
+            subSet(nums, i + 1, ans, list);
+            list.remove(list.size() - 1);
+        }
+    }
+
+
+}
