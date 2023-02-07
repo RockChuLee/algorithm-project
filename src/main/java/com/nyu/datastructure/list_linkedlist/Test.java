a067f95954cf34fba5eea570f27c71c48f634b2e
Hao Dong <13132193357@163.com>
2023-02-06 21:24:15 -0500

    1

---
+++ src/main/java/com/nyu/datastructure/list_linkedlist/Test.java
@@ -0,0 +1,45 @@
+package com.nyu.datastructure.list_linkedlist;
+
+import com.nyu.datastructure.list_linkedlist.util.ListNode;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+
+    public static LinkedListCycle linkedListCycle = new LinkedListCycle();
+    public static ReverseLinkedList reverseLinkedList = new ReverseLinkedList();
+    public static RemoveDuplicatesfromSortedList2 removeDuplicatesfromSortedList2 = new RemoveDuplicatesfromSortedList2();
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,3,4,4,5);
+        ListNode head = initListNode(list);
+
+        ListNode listNode = removeDuplicatesfromSortedList2.deleteDuplicates(head);
+
+        System.out.println(toListNodeString(listNode));
+    }
+
+    // initial a linkedlist
+    private static ListNode initListNode(List<Integer> list) {
+        ListNode head = new ListNode();
+        ListNode dummy = head;
+        for (int i = 0; i < list.size(); i++) {
+            ListNode temp = new ListNode(list.get(i),null);
+            head.next = temp;
+            head = temp;
+        }
+        return dummy.next;
+    }
+
+    // print a linkedlist
+    private static String toListNodeString(ListNode head) {
+        StringBuilder stringBuilder = new StringBuilder();
+        while (head!= null) {
+            stringBuilder.append(head.val).append("->");
+            head = head.next;
+        }
+        return stringBuilder.substring(0,stringBuilder.length()-2);
+    }
+
+}
--- src/main/java/com/nyu/datastructure/list_linkedlist/util/Test.java
+++ src/main/java/com/nyu/datastructure/list_linkedlist/util/Test.java
@@ -9,17 +9,7 @@ import java.util.List;
 
 public class Test {
 
-    public static LinkedListCycle linkedListCycle = new LinkedListCycle();
-    public static ReverseLinkedList reverseLinkedList = new ReverseLinkedList();
-    public static RemoveDuplicatesfromSortedList2 removeDuplicatesfromSortedList2 = new RemoveDuplicatesfromSortedList2();
-
     public static void main(String[] args) {
-//        List<Integer> list = Arrays.asList(1,2,3,3,4,4,5);
-//        ListNode head = initListNode(list);
-//
-//        ListNode listNode = removeDuplicatesfromSortedList2.deleteDuplicates(head);
-//
-//        System.out.println(toListNodeString(listNode));
         MyLinkedList myLinkedList = new MyLinkedList();
         myLinkedList.ListInsert(myLinkedList, 1, 1);
         myLinkedList.ListInsert(myLinkedList, 2, 2);
@@ -34,27 +24,4 @@ public class Test {
         myLinkedList.GetElem(myLinkedList, 5, 0);
         System.out.println(myLinkedList.returnValue);
     }
-
-    // initial a linkedlist
-    private static ListNode initListNode(List<Integer> list) {
-        ListNode head = new ListNode();
-        ListNode dummy = head;
-        for (int i = 0; i < list.size(); i++) {
-            ListNode temp = new ListNode(list.get(i),null);
-            head.next = temp;
-            head = temp;
-        }
-        return dummy.next;
-    }
-
-    // print a linkedlist
-    private static String toListNodeString(ListNode head) {
-        StringBuilder stringBuilder = new StringBuilder();
-        while (head!= null) {
-            stringBuilder.append(head.val).append("->");
-            head = head.next;
-        }
-        return stringBuilder.substring(0,stringBuilder.length()-2);
-    }
-
 }
