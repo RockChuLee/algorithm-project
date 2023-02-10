d275cc58b4ed86ddff540f3445710fd28eef149a
Hao Dong <13132193357@163.com>
2023-02-09 23:43:48 -0500

    1

--- src/main/java/com/nyu/test/DailyQuestion.java
+++ src/main/java/com/nyu/test/DailyQuestion.java
@@ -1,9 +1,8 @@
 package com.nyu.test;
 
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Stack;
 
 public class DailyQuestion {
 
@@ -27,6 +26,29 @@ public class DailyQuestion {
     public int add(int val) {
         heap.offer(val);
         return heap.poll();
+    }
+
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) return false;
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (stack.isEmpty() && (c == ')' || c == '}' || c == ']')) {
+                return false;
+            } else {
+                if (!stack.isEmpty()) {
+                    if (stack.peek() == '(' && c == ')'
+                            || stack.peek() == '{' && c == '}'
+                            || stack.peek() == '[' && c == ']') {
+                        stack.pop();
+                    } else {
+                        stack.push(c);
+                    }
+                } else {
+                    stack.push(c);
+                }
+            }
+        }
+        return stack.isEmpty();
     }
 
     public static void main(String[] args) {
