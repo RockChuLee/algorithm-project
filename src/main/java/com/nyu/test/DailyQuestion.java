29bee110accfd797ad751f0464468d7bad557b2f
Hao Dong <13132193357@163.com>
2023-02-12 18:59:19 -0500

    1

--- src/main/java/com/nyu/test/DailyQuestion.java
+++ src/main/java/com/nyu/test/DailyQuestion.java
@@ -1,62 +1,24 @@
 package com.nyu.test;
 
+import java.util.Arrays;
 
-import java.util.PriorityQueue;
-import java.util.Stack;
-
-public class DailyQuestion {
-
-    PriorityQueue<Integer> heap = new PriorityQueue<>();
-    int k ;
-    int[] nums;
-
-    public DailyQuestion(int k, int[] nums) {
-        this.k = k;
-        this.nums = nums;
-        for(int n:nums){
-            if(heap.size()<k){
-                heap.offer(n);
-            }else{
-                heap.offer(n);
-                heap.poll();
-            }
-        }
-    }
-
-    public int add(int val) {
-        heap.offer(val);
-        return heap.poll();
-    }
-
-    public boolean isValid(String s) {
-        if (s.length() % 2 != 0) return false;
-        Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (stack.isEmpty() && (c == ')' || c == '}' || c == ']')) {
-                return false;
-            } else {
-                if (!stack.isEmpty()) {
-                    if (stack.peek() == '(' && c == ')'
-                            || stack.peek() == '{' && c == '}'
-                            || stack.peek() == '[' && c == ']') {
-                        stack.pop();
-                    } else {
-                        stack.push(c);
-                    }
-                } else {
-                    stack.push(c);
-                }
-            }
-        }
-        return stack.isEmpty();
-    }
-
+class DailyQuestion {
     public static void main(String[] args) {
-        DailyQuestion dailyQuestion = new DailyQuestion(3, new int[]{4, 5, 8, 2});
-        dailyQuestion.add(3);
-        dailyQuestion.add(5);
-        dailyQuestion.add(10);
-        dailyQuestion.add(9);
-        dailyQuestion.add(4);
+
+    }
+
+    public int[][] substringXorQueries(String s, int[][] queries) {
+        int[][] ans = new int[queries.length][2];
+        for (int i = 0; i < queries.length; i++) {
+            int target = queries[i][0] ^ queries[i][1];
+            String t = Integer.toBinaryString(target);
+            if (s.contains(t)) {
+                int index = s.indexOf(t);
+                ans[i] = new int[]{index, index+ t.length()};
+            }else{
+                ans[i] = new int[]{-1, -1};
+            }
+        }
+        return ans;
     }
 }
