905b3add4d2ecfa46855ec92b0d71c4ab782541a
Hao Dong <13132193357@163.com>
2023-02-15 00:11:53 -0500

    DailyQuestion

--- src/main/java/com/nyu/test/DailyQuestion.java
+++ src/main/java/com/nyu/test/DailyQuestion.java
@@ -1,24 +1,10 @@
 package com.nyu.test;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 class DailyQuestion {
-    public static void main(String[] args) {
 
-    }
-
-    public int[][] substringXorQueries(String s, int[][] queries) {
-        int[][] ans = new int[queries.length][2];
-        for (int i = 0; i < queries.length; i++) {
-            int target = queries[i][0] ^ queries[i][1];
-            String t = Integer.toBinaryString(target);
-            if (s.contains(t)) {
-                int index = s.indexOf(t);
-                ans[i] = new int[]{index, index+ t.length()};
-            }else{
-                ans[i] = new int[]{-1, -1};
-            }
-        }
-        return ans;
-    }
 }
+
