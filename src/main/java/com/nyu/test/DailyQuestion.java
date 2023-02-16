e6515d093804a3979b9957e80ae433e62a436c45
Hao Dong <13132193357@163.com>
2023-02-16 00:55:43 -0500

    modify DailyQuestion.java

--- src/main/java/com/nyu/test/DailyQuestion.java
+++ src/main/java/com/nyu/test/DailyQuestion.java
@@ -1,10 +1,30 @@
 package com.nyu.test;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.Collections;
 
 class DailyQuestion {
+    private static int extracted() {
+        int n = 1;
+        n = n - ((n >>> 1) & 0x55555555);
+        n = (n & 0x33333333) + ((n >>> 2) & 0x33333333);
+        n = (n + (n >>> 4)) & 0x0f0f0f0f;
+        n = n + (n >>> 8);
+        n = n + (n >>> 16);
+        return n & 0x3f;
+    }
 
+    public  static int reverseBits(int n) {
+        int res = 1;
+        while(n!=0){
+            res |=  n & 1 ;
+            n  = n >> 1;
+            res = res<< 1;
+        }
+        return res >> 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
 
