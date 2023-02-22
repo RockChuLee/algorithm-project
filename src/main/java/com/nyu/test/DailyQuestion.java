a10d1b7e5d4197b1f4e7bb289ed273e2dbdd8420
Hao Dong <13132193357@163.com>
2023-02-22 16:09:14 -0500

    modify DailyQuestion.java

--- src/main/java/com/nyu/test/DailyQuestion.java
+++ src/main/java/com/nyu/test/DailyQuestion.java
@@ -1,8 +1,5 @@
 package com.nyu.test;
 
-import javax.xml.transform.Source;
-import java.util.*;
-
 class DailyQuestion {
     private static int extracted() {
         int n = 1;
@@ -28,27 +25,58 @@ class DailyQuestion {
         int valid = 0;
         int star = 0;
         for(int i = 0;i<s.length();i++){
-            switch(s.charAt(i)){
+            switch (s.charAt(i)) {
                 case '(':
                     valid++;
                     break;
                 case '*':
-                    star ++ ;
+                    star++;
                     break;
                 case ')':
-                    if(valid>0){
-                        valid --;
-                    }else{
+                    if (valid > 0) {
+                        valid--;
+                    } else {
                         return false;
                     }
                     break;
             }
         }
-        return valid<=star;
+        return valid <= star;
+    }
+
+    static int[][] dirs = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    static int result = 0;
+    static int n = 0;
+    static int m = 0;
+    static char[][] target;
+
+    public static int numIslands(char[][] grid) {
+        n = grid.length;
+        m = grid[0].length;
+        target = grid;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (target[i][j] == '1') {
+                    result++;
+                }
+                dfs(i, j);
+            }
+        }
+        return result;
+    }
+
+    public static void dfs(int i, int j) {
+        if (i >= n || j >= m || i < 0 || j < 0 || target[i][j] == '0') {
+            return;
+        }
+        target[i][j] = '0';
+        for (int[] dir : dirs) {
+            dfs(i + dir[0], j + dir[1]);
+        }
     }
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<Integer>(Collections.singletonList(1));
+        numIslands(new char[][]{{'1', '1', '0', '0', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '1', '0', '0'}, {'0', '0', '0', '1', '1'}});
     }
 
     private static void extracted1() {
